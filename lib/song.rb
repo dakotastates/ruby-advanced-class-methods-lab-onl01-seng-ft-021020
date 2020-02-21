7facbb02e8c6eaeb4010f81c1af611e438e068aa
Dakota States <dakota.states@gmail.com>
2020-02-21 16:32:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,12 @@ class Song
   def save
     self.class.all << self
   end
+ 
   def self.create
     song = self.new 
     song
   end
+  
   def self.new_by_name(name)
     song = self.new 
     song.name = name
